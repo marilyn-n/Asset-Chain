80b280e58f9ae126c364b787bec36b665b7891eb
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-04-01 14:28:59 -0500

    fixed conflict

--- server/routes/testamentController.js
+++ server/routes/testamentController.js
@@ -1,90 +1,70 @@
 const express = require('express');
-const router  = express.Router();
-const User    = require("../models/User.js");
-const Testament    = require("../models/Testament.js");
-const Asset = require ("../models/Asset.js")
-const EmailService = require('../models/EmailService')
-const driver = require('bigchaindb-driver')
 
-router.get('/all', (req,res,next)=> {
+const router = express.Router();
+const User = require('../models/User.js');
+const Testament = require('../models/Testament.js');
+const Asset = require('../models/Asset.js');
+const EmailService = require('../models/EmailService');
+// const driver = require('bigchaindb-driver')
+
+router.get('/all', (req, res) => {
   Testament.find()
-    .then(allTestaments => res.status(200).json(allTestaments))
-})
+    .then(allTestaments => res.status(200).json(allTestaments));
+});
 
 // send testament on death with blockchain methods(not ready)
-router.post('/new', (req, res, next) =>{
-  console.log(req.user)
-    const myTestament = new Testament({
-     owner: req.user._id,
-     description: req.body.description,
-     executorEmail: req.body.executorEmail,
-   });
-
-   myTestament.save()
-    .then(result =>{
-        hackToUpdateUser(req.user._id, result._id);
-        res.json(result);
-      })
-    .catch(err => res.status(400).json({message:err}))
+router.post('/new', (req, res) => {
+  console.log(req.user);
+  const myTestament = new Testament({
+    owner: req.user._id,
+    description: req.body.description,
+    executorEmail: req.body.executorEmail,
   });
 
-let bdb = new driver.Connection('https://test.bigchaindb.com/api/v1/', {
-        app_id: 'b5324ac9',
-        app_key: 'd06aea79d7643db7c33a8993a4140f5f'
-})
+  myTestament.save()
+    .then((result) => {
+      hackToUpdateUser(req.user._id, result._id);
+      res.json(result);
+    })
+    .catch(err => res.status(400).json({ message: err }));
+});
 
-const secret = new driver.Ed25519Keypair()        
-const text = myTestament.description
-const metadata = {'user': 'jeff bezos'}
+// let bdb = new driver.Connection('https://test.bigchaindb.com/api/v1/', {
+//         app_id: 'b5324ac9',
+//         app_key: 'd06aea79d7643db7c33a8993a4140f5f'
+// })
 
-const txCreateData = driver.Transaction.makeCreateTransaction(
-        text, metadata,
-    [ driver.Transaction.makeOutput(
-            driver.Transaction.makeEd25519Condition(secret.publicKey))
-    ],
-    secret.publicKey
-)
+// const secret = new driver.Ed25519Keypair()
+// const text = myTestament.description
+// const metadata = {'user': 'jeff bezos'}
 
-const txCreateDataSigned = driver.Transaction.signTransaction(txCreateData, secret.privateKey)
+// const txCreateData = driver.Transaction.makeCreateTransaction(
+//         text, metadata,
+//     [ driver.Transaction.makeOutput(
+//             driver.Transaction.makeEd25519Condition(secret.publicKey))
+//     ],
+//     secret.publicKey
+// )
 
-bdb.postTransaction(txCreateDataSigned)
+// const txCreateDataSigned = driver.Transaction.signTransaction(txCreateData, secret.privateKey)
 
-function hackToUpdateUser(userId, testamentId){
-  User.findByIdAndUpdate(userId, {testament:testamentId}, {new:true})
-  .then(res=>console.log('ok',res));
+// bdb.postTransaction(txCreateDataSigned)
+
+function hackToUpdateUser(userId, testamentId) {
+  User.findByIdAndUpdate(userId, { testament: testamentId }, { new: true })
+    .then(res => console.log('ok', res));
 }
 
-// show testament 
-//ruta por la cual , nos devuelva el testamento y los bienes relacionados con la misma.
 router.get('/testament-details', (req, res) => {
-  // console.log("dentro de details")
-  // Testament.find({owner:req.user._id})
-  // .populate('assets')
-  // .then(result1 => {
-  //   console.log(result1)
-  //   Asset.find({idTestament: result1._id})
-  //     .then(result2 =>  {
-  //       console.log(result2)
-  //       const resultTotal= {
-  //         testament:result1,
-  //         assets:result2
-  //       }
-  //       res.status(200).json(resultTotal)
-  //     })
-  // })
 
   Testament.findById(req.user.testament)
-  .populate('assets')
-  .then(testament=>res.json(testament))
-  .catch(e=>res.json(e))
-})
-
-
-router.post('/payment-method',(req, res) => {
-  return res.status(403).json({ message: 'payment method' });
+    .populate('assets')
+    .then(testament => res.json(testament))
+    .catch(e => res.json(e));
 });
 
 
+router.post('/payment-method', (req, res) => res.status(403).json({ message: 'payment method' }));
+
+
 module.exports = router;
-
-
