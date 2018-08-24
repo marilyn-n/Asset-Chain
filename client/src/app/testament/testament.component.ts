d5fbccf50102b1008530f0401d87cf3d57540083
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-08-24 10:56:42 -0500

    removed unnecessary comments and spaces

--- client/src/app/testament/testament.component.ts
+++ client/src/app/testament/testament.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { TestamentService } from '../services/testament.service';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-testament',
   templateUrl: './testament.component.html',
@@ -15,17 +14,10 @@ export class TestamentComponent implements OnInit {
   user;
 
   submitForm(newForm) {
-    //console.log(newForm.value);
-    //this.user = JSON.parse(localStorage.getItem('user'));
-    //newForm.value['owner'] = this.user._id
-    //console.log(newForm);
-
-   this.testamentS.postNewTestament(newForm.value)
-    .subscribe( testamentCreated =>{
+    this.testamentS.postNewTestament(newForm.value)
+    .subscribe(testamentCreated => {
       localStorage.setItem('testament', JSON.stringify(testamentCreated));
-      //console.log(testamentCreated);
       this.router.navigate(['/assets/benefitiaries', testamentCreated._id])
     });
   }
-
 }
