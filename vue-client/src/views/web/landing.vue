6f2340a3702d2a683015ba107657084fbc9f61b1
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-10-22 19:47:18 -0500

    initialize landing

---
+++ vue-client/src/views/web/landing.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img src="@/assets/img/ac-logo.png" alt="">
+    <p>Asset Chain landing</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
