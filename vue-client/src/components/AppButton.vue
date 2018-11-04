a6b1f8c750a87a24b7897c9d0ba5dd55451022fc
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-11-04 15:43:25 -0600

    create AppButton component

---
+++ vue-client/src/components/AppButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="action">
+    title
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+button {
+  background-color: black;
+  color: white;
+  padding: 20px 100px;
+  font-size: 16px;
+  border-color: black;
+}
+
+</style>
--- vue-client/src/views/web/landing.vue
+++ vue-client/src/views/web/landing.vue
@@ -1,16 +1,48 @@
 <template>
-  <div>
+  <div class="landing">
+    <video src="@/assets/video/Empire.mp4" autoplay loop></video>
     <img src="@/assets/img/ac-logo.png" alt="">
-    <p>Asset Chain landing</p>
+    <app-button
+      class="get-started"
+      >
+    </app-button>
+    
   </div>
 </template>
 
 <script>
 export default {
+  components: {
+    AppButton: () => import('@/components/AppButton')
+  },
+  data () {
+    return {
+
+    }
+  }
 
 };
 </script>
 
-<style>
+<style scoped>
 
+img {
+  position: absolute;
+  top: 400px;
+  left: 450px;
+  height: 115px;
+  width: 300px;
+}
+
+video{
+  width: 1420px;
+  height: 780;
+  margin: 0;
+}
+
+.get-started {
+  position: absolute;
+  top: 400px;
+  left: 750px;
+}
 </style>
